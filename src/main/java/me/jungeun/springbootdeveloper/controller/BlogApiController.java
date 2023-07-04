d014e41d509cab2a64e7d797f30b2138e6fb44c5
Yun-tellije <dbswjddms517@gmail.com>
2023-07-04 18:23:59 +0900

    [Comment] 컨트롤러 주석 추가

--- src/main/java/me/jungeun/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/jungeun/springbootdeveloper/controller/BlogApiController.java
@@ -12,14 +12,23 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequiredArgsConstructor // TODO
-@RestController // TODO
+@RequiredArgsConstructor
+// 초기화 되지않은 final 필드나, @NonNull이 붙은 필드에 대해 생성자를 생성
+// DI에는 필드 주입, 수정자 주입, 생성자 주입 3가지 방법이 존재
+// 생성자 주입을 위한 코드는 번거로움이 존재 -> 그래서 이거 사용
+
+@RestController
+// @Controller + @ResponseBody
+// Json 형태로 객체 데이터를 반환하는 용도로 사용
+
 public class BlogApiController {
 
     private final BlogService blogService;
 // new String("sdf", "sdf)
     // new String().title("sdf").contend("sdf)
     @PostMapping("/api/articles") // TODO
+    // HTTP Post Method에 해당하는 단축 표현, 서버에 리소스를 등록할 때 사용
+
     // 새로운 글 추가. 요청 바디에는 'AddArticleRequest' 객체가 필요
     public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) { // RequestBody -> dto
         Article savedArticle = blogService.save(request);
@@ -29,6 +38,8 @@ public class BlogApiController {
     }
 
     @GetMapping("/api/articles") // TODO
+    // HTTP Get Method에 해당하는 단축 표현, 서버의 리소스를 조회할 때 사용
+
     // 모든 글 조회. 'ArticleResponse' 객체의 리스트 반환
     public ResponseEntity<List<ArticleResponse>> findAllArticles() {
         List<ArticleResponse> articles = blogService.findAll()
@@ -40,6 +51,8 @@ public class BlogApiController {
                 .body(articles);
     }
     @GetMapping("/api/articles/{id}")
+    // HTTP Get Method에 해당하는 단축 표현, 서버의 리소스를 조회할 때 사용
+
     // 특정 ID의 글을 조회. 'ArticleResponse' 객체를 반환
     public ResponseEntity<ArticleResponse> findArticle(@PathVariable long id) {
         Article article = blogService.findById(id);
