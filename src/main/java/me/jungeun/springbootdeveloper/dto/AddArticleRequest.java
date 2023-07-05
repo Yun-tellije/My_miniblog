df5e53e6bd653c568248063dee0ba77df10c90c8
Yun-tellije <dbswjddms517@gmail.com>
2023-07-05 21:10:56 +0900

    [Comment] DTO(AddArticleRequest) 주석 추가 마무리

--- src/main/java/me/jungeun/springbootdeveloper/dto/AddArticleRequest.java
+++ src/main/java/me/jungeun/springbootdeveloper/dto/AddArticleRequest.java
@@ -10,8 +10,10 @@ import me.jungeun.springbootdeveloper.domain.Article;
 // 모든 필드 값을 파라미터로 받는 생성자를 만들어줌
 
 @NoArgsConstructor // TODO
+// 파라미터가 없는 기본 생성자를 생성
 
 @Getter // TODO
+// get 메소드 생성
 public class AddArticleRequest {
 
     private String title;
