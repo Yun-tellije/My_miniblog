ae9304c99a9dfba4fb08224731ecd1e656efd112
Yun-tellije <dbswjddms517@gmail.com>
2023-07-05 21:20:34 +0900

    [Comment] SpringBootDeveloperApplication 주석 추가

--- src/main/java/me/jungeun/springbootdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/me/jungeun/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing // TODO
+// 엔티티 객체가 생성이 되거나 변경 되었을 때 @EnableJpaAuditing
+// 어노테이션으 활용하여 자동으로 값을 등록할 수 있다
 @SpringBootApplication // TODO
 public class SpringBootDeveloperApplication {
     public static void main(String[] args) {
