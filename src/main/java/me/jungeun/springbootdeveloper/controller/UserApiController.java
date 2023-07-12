9d9047e328106c110a54709b42ce0242f92e3b28
jeun0517 <dbswjddms517@naver.com>
2023-07-12 20:32:13 +0900

    [Feat] UserApiController 클래스 추가

---
+++ src/main/java/me/jungeun/springbootdeveloper/controller/UserApiController.java
@@ -0,0 +1,20 @@
+package me.jungeun.springbootdeveloper.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.jungeun.springbootdeveloper.dto.AddUserRequest;
+import me.jungeun.springbootdeveloper.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class UserApiController {
+
+    private final UserService userService;
+
+    @PostMapping("/user")
+    public String signup(AddUserRequest request){
+        userService.save(request);
+        return "redirect:/login";
+    }
+}
