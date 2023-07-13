bf4bf5ddca09eb07a7a55232110c749ce0ab7f66
Yun-tellije <dbswjddms517@gmail.com>
2023-07-13 18:20:34 +0900

    [Feat] 본인 글이 아닐 때 예외 발생시키도록 수정

--- src/main/java/me/jungeun/springbootdeveloper/service/BlogService.java
+++ src/main/java/me/jungeun/springbootdeveloper/service/BlogService.java
@@ -7,6 +7,7 @@ import me.jungeun.springbootdeveloper.dto.AddArticleRequest;
 import me.jungeun.springbootdeveloper.dto.ArticleResponse;
 import me.jungeun.springbootdeveloper.dto.UpdateArticleRequest;
 import me.jungeun.springbootdeveloper.repository.BlogRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -34,6 +35,9 @@ public class BlogService {
     }
 
     public void delete(long id) {
+        Article article = blogRepository.findById(id)
+                        .orElseThrow(() -> new IllegalArgumentException("not found : " + id));
+        authorizeArticleAuthor(article);
         blogRepository.deleteById(id);
     }
 
@@ -44,8 +48,14 @@ public class BlogService {
         Article article = blogRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("not found : " + id));
 
+        authorizeArticleAuthor(article);
         article.update(request.getTitle(), request.getContent());
 
         return article;
+    }
+
+    // 게시글을 작성한 유저인지 확인
+    private static void authorizeArticleAuthor(Article article){
+        String userName = SecurityContextHolder.getContext().getAuthentication().getName();
     }
 }
